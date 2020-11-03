f1fb355ecd8f2ac1ee821865af375b913f3ea514
Justin Davidson <justindavidson45@gmail.com>
2020-11-02 20:23:25 -0600

    fixed buttons on botom

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -5,6 +5,7 @@ import GitHubIcon from "@material-ui/icons/GitHub"
 import "./layout.css"
 import EmailIcon from "@material-ui/icons/Email"
 import Slide from "@material-ui/core/Slide"
+import { IconButton } from '@material-ui/core';
 
 class Buttons extends React.Component {
   render() {
@@ -13,29 +14,15 @@ class Buttons extends React.Component {
         <Slide direction="up" in="true" timeout={1000}>
           <div className="myinfo">
             <div className="outsideinfo">
-              <div className="innermyinfo">
-                <div className="linktome">
-                  <Button
-                    className="linkedIn"
-                    variant="link"
-                    color="primary"
-                    startIcon={<LinkedInIcon />}
-                    href="https://www.linkedin.com/in/jdavidson45/"
-                  />
-                  <Button
-                    variant="link"
-                    color="default"
-                    startIcon={<GitHubIcon />}
-                    href="https://github.com/JDavidson45/"
-                  />
-                  <Button
-                    variant="link"
-                    color="default"
-                    startIcon={<EmailIcon />}
-                    href="mailto:justindavidson45@gmail.com"
-                  />
-                </div>
-              </div>
+              {/* <div className="innermyinfo"> */}
+                {/* <div className="linktome"> */}
+                  <div style={{display: 'flex', justifyContent: 'center', margin: '15px'}}>
+                <IconButton  className="linkedIn" href="https://www.linkedin.com/in/jdavidson45/"><LinkedInIcon /></IconButton>
+                  <IconButton href="https://github.com/JDavidson45/"><GitHubIcon /></IconButton>
+                  <IconButton href="mailto:justindavidson45@gmail.com"><EmailIcon /></IconButton>
+                  </div>
+                {/* </div> */}
+              {/* </div> */}
             </div>
           </div>
         </Slide>
--- src/pages/Earbuds.js
+++ src/pages/Earbuds.js
@@ -1,25 +1,32 @@
 import React from "react"
 import { Link } from "gatsby"
-import vid from '../../public/static/vid.mp4'
+import vid from "../../public/static/vid.mp4"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Button } from 'react-bootstrap';
-import Header from "../components/header";
-import Buttons from "../components/Buttons";
-import { flexbox } from "@material-ui/system";
-import LinkIcon from '@material-ui/icons/Link';
+import { Button } from "@material-ui/core"
+import Header from "../components/header"
+import Buttons from "../components/Buttons"
+import { flexbox } from "@material-ui/system"
+import LinkIcon from "@material-ui/icons/Link"
+import { IconButton } from '@material-ui/core';
 const Earbuds = () => (
   <div>
-  <Header />
-  <div className='video'>
-    <h1 className='welcome'>Welcome to Earbuds!</h1>
-    <video  src={vid} width="600" height="300" controls="controls" autoplay="true" />
-    <br/>
-     <LinkIcon><button href="https://earbuds.web.app"></button></LinkIcon>
+    <Header />
+    <div className="video">
+      <h1 className="welcome">Welcome to Earbuds!</h1>
+      <video
+        src={vid}
+        width="600"
+        height="300"
+        controls="controls"
+        autoplay="true"
+      />
+      <br />
+      <IconButton href="https://earbuds.web.app"><LinkIcon /></IconButton>
     </div>
 
-     <Buttons />
-    </div>
+    <Buttons />
+  </div>
 )
 
 export default Earbuds

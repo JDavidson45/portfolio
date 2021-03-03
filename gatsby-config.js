5ae788bf772a53b84991c11b98e2fbc984707af8
Justin Davidson <justindavidson45@gmail.com>
2021-03-02 21:14:39 -0600

    deploying

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#282828`,
         theme_color: `#282828`,
         display: `minimal-ui`,
-        icon: `src/images/logo.png`, // This path is relative to the root of the site.
+        icon: `public/static/logo.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
